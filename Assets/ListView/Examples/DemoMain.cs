67de7602ea905ca6506792a8544a12f2f46e8187
yicheng55 <l5977951@gmail.com>
2021-08-07 21:06:37 +0800

    Socket added.

--- Assets/ListView/Examples/DemoMain.cs
+++ Assets/ListView/Examples/DemoMain.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Text;
 
 public class DemoMain : MonoBehaviour
 {
@@ -9,7 +15,15 @@ public class DemoMain : MonoBehaviour
     public DemoItem itemVPrefab;
     public DemoItem itemHPrefab;
 
-    private void Update()
+	public InputField[] tbx_Txt;
+
+	#region private members
+	private TcpClient socketConnection;
+    private Thread clientReceiveThread;
+	#endregion
+	private string serverMessage;
+
+	private void Update()
     {
         if (Input.GetKeyDown(KeyCode.V))
         {
@@ -19,37 +33,69 @@ public class DemoMain : MonoBehaviour
             }
             else // v: add
             {
-                AddItem(listViewVertical, itemVPrefab);
+                AddItem(listViewVertical, itemVPrefab, serverMessage);
             }
         }
 
-        //if (Input.GetKeyDown(KeyCode.H))
-        //{
-        //    if (Input.GetKey(KeyCode.LeftShift)) // shift + h: remove
-        //    {
-        //        RemoveItem(listViewHorizontal);
-        //    }
-        //    else // h: add
-        //    {
-        //        AddItem(listViewHorizontal, itemHPrefab);
-        //    }
-        //}
-    }
+		//if (Input.GetKeyDown(KeyCode.H))
+		//{
+		//    if (Input.GetKey(KeyCode.LeftShift)) // shift + h: remove
+		//    {
+		//        RemoveItem(listViewHorizontal);
+		//    }
+		//    else // h: add
+		//    {
+		//        AddItem(listViewHorizontal, itemHPrefab);
+		//    }
+		//}
 
-    private void AddItem(ListView lv, DemoItem prefab)
+		if (serverMessage != null)
+		{
+			AddItem(listViewVertical, itemVPrefab, serverMessage);
+			serverMessage = null;
+		}
+	}
+
+	public void ConnectButton()
+	{
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+		ConnectToTcpServer();
+	}
+
+	//public void SendMessageButton()
+	//{
+	//	//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+	//	SendMessage();
+	//}
+
+	public void SendButton(int x)
+	{
+		AddItem(listViewVertical, itemVPrefab,null);
+		Debug.Log("SendButton= " + x);
+	}
+	private void AddItem(ListView lv, DemoItem prefab, string msg)
     {
         var color = new Color()
         {
-            r = Random.Range(0.0f, 1.0f),
-            g = Random.Range(0.0f, 1.0f),
-            b = Random.Range(0.0f, 1.0f),
+            r = UnityEngine.Random.Range(0.0f, 1.0f),
+            g = UnityEngine.Random.Range(0.0f, 1.0f),
+            b = UnityEngine.Random.Range(0.0f, 1.0f),
             a = 1.0f,
         };
 
         var item = Instantiate(prefab);
-        item.SetContent(color.ToString(), color);
 
-        lv.AddItem(item.gameObject);
+		if (serverMessage == null)
+		{
+            item.SetContent(color.ToString(), color);
+        }
+		else
+        {
+			item.SetContent(msg, color);
+		}
+
+
+		lv.AddItem(item.gameObject);
         Debug.Log("lv.ItemCount= " + lv.ItemCount);
     }
 
@@ -59,4 +105,86 @@ public class DemoMain : MonoBehaviour
         lv.RemoveTop();
         Debug.Log("lv.ItemCount= " + lv.ItemCount);
     }
+
+
+	/// <summary>
+	/// Setup socket connection.
+	/// </summary>
+	private void ConnectToTcpServer()
+	{
+		try
+		{
+			clientReceiveThread = new Thread(new ThreadStart(ListenForData));
+			clientReceiveThread.IsBackground = true;
+			clientReceiveThread.Start();
+			Debug.Log("clientReceiveThread.Start(); ");
+		}
+		catch (Exception e)
+		{
+			Debug.Log("On client connect exception " + e);
+		}
+	}
+	/// <summary>
+	/// Runs in background clientReceiveThread; Listens for incomming data.
+	/// </summary>
+	private void ListenForData()
+	{
+		try
+		{
+			socketConnection = new TcpClient("localhost", 8052);
+			Byte[] bytes = new Byte[1024];
+			while (true)
+			{
+				// Get a stream object for reading
+				using (NetworkStream stream = socketConnection.GetStream())
+				{
+					int length;
+					// Read incomming stream into byte arrary.
+					while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+					{
+						var incommingData = new byte[length];
+						Array.Copy(bytes, 0, incommingData, 0, length);
+						// Convert byte array to string message.
+						serverMessage = Encoding.ASCII.GetString(incommingData);
+						Debug.Log("server message received as: " + serverMessage);
+
+					}
+				}
+			}
+		}
+		catch (SocketException socketException)
+		{
+			Debug.Log("Socket exception: " + socketException);
+		}
+	}
+	/// <summary>
+	/// Send message to server using socket connection.
+	/// </summary>
+	private void SendMessage()
+	{
+		if (socketConnection == null)
+		{
+			return;
+		}
+		try
+		{
+			// Get a stream object for writing.
+			NetworkStream stream = socketConnection.GetStream();
+			if (stream.CanWrite)
+			{
+				string clientMessage = "This is a message from one of your clients.";
+				// Convert string message to byte array.
+				byte[] clientMessageAsByteArray = Encoding.ASCII.GetBytes(clientMessage);
+				// Write byte array to socketConnection stream.
+				stream.Write(clientMessageAsByteArray, 0, clientMessageAsByteArray.Length);
+				Debug.Log("Client sent his message - should be received by server");
+				//textView.text = clientMessage;
+			}
+		}
+		catch (SocketException socketException)
+		{
+			Debug.Log("Socket exception: " + socketException);
+		}
+	}
+
 }
